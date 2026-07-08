0a6de1fc05811ef797164d956fe25ea435c66937
Juan upb desktop <juanf.franco@upb.edu.co>
2026-07-08 14:33:50 -0500

    2026-20 setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sistemas Físicos Interactivos 1 2026-20
+
+Sitio web: [https://juanferfranco.github.io/interactivos1-2026-20](https://juanferfranco.github.io/interactivos1-2026-20/)
---
+++ astro.config.mjs
@@ -0,0 +1,56 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import remarkMath from 'remark-math';
+import rehypeMathjax from 'rehype-mathjax';
+
+// https://astro.build/config
+export default defineConfig({
+	base: '/interactivos1-2026-20',
+	markdown: {
+		syntaxHighlight: {
+			type: 'shiki',
+			excludeLangs: ['math'],
+		},
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeMathjax],
+	},
+	integrations: [
+		starlight({
+			title: 'Sistemas Físicos Interactivos 1',
+			defaultLocale: 'root',
+			locales: {
+				root: { label: 'Español', lang: 'es-ES' },
+			},
+			customCss: ['./src/styles/custom.css'],
+			sidebar: [
+				{
+					label: 'Introducción',
+					items: [
+						{ label: '¿De qué se trata este curso?', slug: 'units/intro' },
+					],
+				},
+				{
+					label: 'Unidades',
+					items: [
+						{ label: 'U1 - Audio', slug: 'units/unit1' },
+						{ label: 'U2 - Visuales', slug: 'units/unit2' },
+						{ label: 'U3 - Integración audio-visual', slug: 'units/unit3' },
+						{ label: 'U4 - Superficie de control', slug: 'units/unit4' },
+						{ label: 'U5 - Input del performer', slug: 'units/unit5' },
+						{ label: 'U6 - Creación', slug: 'units/unit6' },
+						{ label: 'U7 - Performance', slug: 'units/unit7' },
+						{ label: 'U8 - Síntesis', slug: 'units/unit8' },
+					],
+				},
+				{
+					label: 'Recursos',
+					items: [
+						{ label: 'Recursos interesantes', slug: 'units/resources' },
+					],
+				},
+			],
+		}),
+	],
+	site: 'https://jfUPB.github.io',
+});
